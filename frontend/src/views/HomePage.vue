75e9861b21f8393901db0eac6e1a3be0abebdff8
陈星宇 <125285397+HarvestMoons@users.noreply.github.com>
2025-12-04 07:36:24 +0800

    解决报错 filteredPlaylist.value is not iterable：页面刷新 playerRef 还没准备好，Playlist 可以拿到一个空数组

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -4,11 +4,11 @@
       <Sidebar />
 
       <!-- 左侧歌单 -->
-      <Playlist
-          :playlist="playerRef?.playlist"
-          :currentSongId="playerRef?.playlist[playerRef?.currentIndex]?.id"
+        <Playlist
+          :playlist="safePlaylist"
+          :currentSongId="currentSongId"
           @select="handlePlaylistClick"
-      />
+        />
 
       <!-- 右侧：播放器 + 频谱 -->
       <div class="right-container">
@@ -25,7 +25,7 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 import Player from '@/components/feature/player/Player.vue'
 import Playlist from '@/components/feature/player/Playlist.vue'
 import Sidebar from '@/components/layout/Sidebar.vue'
@@ -35,11 +35,17 @@ import MySpinner from "@/components/effects/MySpinner.vue";
 
 const playerRef = ref(null)
 
+const safePlaylist = computed(() => playerRef.value?.playlist ?? [])
+const currentSongId = computed(() => {
+  const playlistInstance = playerRef.value?.playlist
+  const index = playerRef.value?.currentIndex ?? -1
+  if (!Array.isArray(playlistInstance) || index < 0) return null
+  return playlistInstance[index]?.id ?? null
+})
+
 // Playlist 点击播放
 function handlePlaylistClick(songId) {
-  if (playerRef.value) {
-    playerRef.value.handleSelectSong(songId)  // 通过 song.id 找索引播放
-  }
+  playerRef.value?.handleSelectSong(songId)
 }
 </script>
 
